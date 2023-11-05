2d846800637b37c458efb737c928ff7f1293c0ff
Mountok <themountok@gmail.com>
2023-11-05 03:13:29 +0300

    login and password check

--- src/LoginPage/LoginPage.jsx
+++ src/LoginPage/LoginPage.jsx
@@ -1,11 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './LoginPageStyle.css'
 import {HiUserCircle, HiUser} from 'react-icons/hi'
 import {AiFillLock} from 'react-icons/ai'
 import {BiSolidUserPin} from 'react-icons/bi'
 import { useNavigate } from 'react-router-dom'
 const LoginPage = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+
+  const [userlogin,setUserLogin] = useState('');
+  const [userPassword,setUserPassword] = useState('');
+
+  
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    let passIsTrue = false, loginIsTrue = false;
+    if(/[a-zA-Z]/.test(userPassword.charAt(0))){
+      if(/\d/.test(userPassword)){
+        if(userPassword.length > 3){
+          passIsTrue = true
+        }else 
+          alert('Длина пароля меньше 3-х символоф')
+       }
+        else 
+          alert('Пароль пароль должен содержать хотябы одну цифру')
+    }else
+      alert('Пароль неможет начинаться с цифры')
+    
+    const pattern = /^\w+$/;
+    if(!pattern.test(userlogin)) {
+      alert('Логин должен состоять только из букв и цифр')
+    }else{
+      loginIsTrue = true;
+    }
+    if(passIsTrue && loginIsTrue){
+      const sendDatas = {
+        userlogin,
+        userPassword
+      }
+        console.log(sendDatas)
+    }
+    
+  }
+
   return (
     <>
     
@@ -17,16 +54,22 @@ const LoginPage = () => {
 
           <div className="login_form_input">
           <HiUser className="login_form_input_icon"/>
-          <input type="text" />
+          <input type="text"
+          onChange={(e)=>setUserLogin(e.target.value)}
+          value={userlogin}
+          placeholder='Введдите имя' />
 
           </div>
           <p>Пароль</p>
           <div className="login_form_input">
           <AiFillLock className="login_form_input_icon"/>
-          <input type="password"  />
+          <input type="password"
+          onChange={(e)=>setUserPassword(e.target.value)}
+          value={userPassword}
+          placeholder='Введдите пароль' />
           </div>
 
-          <button onClick={()=>navigate('/orgnize')}>Войти</button>
+          <button onClick={(e)=>handleSubmit(e)}>Войти</button>
         </form>
       </div>
     </div>
